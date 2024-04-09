1929a27f8277f342edf447e103bbf62383194c19
CHJun47 <sniper175@kakao.com>
2024-04-09 15:56:01 +0900

    RR

--- login.py
+++ login.py
@@ -7,7 +7,7 @@ from st_supabase_connection import SupabaseConnection
 conn = st.connection("supabase",type=SupabaseConnection)
 
 # Perform query.
-rows = conn.query("*", table="mytable", ttl="10m").execute()
+rows = conn.query("*", table="user", ttl="10m").execute()
 
 # Print results.
 for row in rows.data:
