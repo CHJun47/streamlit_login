cfd2e9c560baec8f805cd4d32f319311e27022d1
CHJun47 <sniper175@kakao.com>
2024-04-09 15:51:42 +0900

    RR

--- login.py
+++ login.py
@@ -1,13 +1,27 @@
 import streamlit as st
 import st_login_form as slf
 # from st_login_form import login_form
+import streamlit as st
+from supabase import create_client, Client
 
-client = slf.login_form()
+# Initialize connection.
+# Uses st.cache_resource to only run once.
+@st.cache_resource
+def init_connection():
+    url = st.secrets["SUPABASE_URL"]
+    key = st.secrets["SUPABASE_KEY"]
+    return create_client(url, key)
 
-if st.session_state["authenticated"]:
-    if st.session_state["username"]:
-        st.success(f"Welcome {st.session_state['username']}")
-    else:
-        st.success("Welcome guest")
-else:
-    st.error("Not authenticated")
+supabase = init_connection()
+
+# Perform query.
+# Uses st.cache_data to only rerun when the query changes or after 10 min.
+@st.cache_data(ttl=600)
+def run_query():
+    return supabase.table("mytable").select("*").execute()
+
+rows = run_query()
+
+# Print results.
+for row in rows.data:
+    st.write(f"{row['name']} has a :{row['pet']}:")
